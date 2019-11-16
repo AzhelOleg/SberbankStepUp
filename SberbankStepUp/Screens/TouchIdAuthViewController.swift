eee20334dfa5ee103843f268942e80b9678501a7
fulstaph <fulstaph@gmail.com>
2019-11-17 02:41:55 +0300

    implemented touch (and face probs) id authentication

--- SberbankStepUp/Screens/TouchIdAuthViewController.swift
+++ SberbankStepUp/Screens/TouchIdAuthViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 public final class TouchIdAuthViewController: UIViewController {
 
@@ -21,11 +22,162 @@ public final class TouchIdAuthViewController: UIViewController {
         let backgroundLayer = gradient.gradientLayer
         backgroundLayer.frame = view.frame
         view.layer.insertSublayer(backgroundLayer, at: 0)
+        
+        authenticationWithTouchId()
     }
     
     override public func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = false
     }
+    
+}
+
+extension TouchIdAuthViewController {
+    func authenticationWithTouchId() {
+        // 1. Create a authentication context
+        let authenticationContext = LAContext()
+        var error:NSError?
+        // 2. Check if the device has a fingerprint sensor
+        // If not, show the user an alert view and bail out!
+        guard authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            showAlertViewIfNoBiometricSensorHasBeenDetected()
+            return
+        }
+        
+        // 3. Check the fingerprint
+        authenticationContext.evaluatePolicy(
+            .deviceOwnerAuthenticationWithBiometrics,
+            localizedReason: "Only awesome people are allowed",
+            reply: { [unowned self] (success, error) -> Void in
+                
+                if success {
+                    // Fingerprint recognized
+                    // Go to view controller
+                    DispatchQueue.global(qos: .background).async {
+                        
+                        // Background Thread
+                        
+                        DispatchQueue.main.async {
+                            // Run UI Updates
+                            AppDelegate.shared.rootViewController.showMainScreen()
+                            
+                        }
+                    }
+
+                } else {
+                    // Check if there is an error
+                    if let error = error {
+                        
+                        //let message = self.errorMessageForLAErrorCode(errorCode: )
+                        self.showAlertViewAfterEvaluatingPolicyWithMessage(message: "things went bad")
+                        
+                    }
+                    
+                }
+        })
+    }
+
+    /**
+     This method will present an UIAlertViewController to inform the user that the device has not a TouchID sensor.
+     */
+    func showAlertViewIfNoBiometricSensorHasBeenDetected(){
+        
+        showAlertWithTitle(title: "Error", message: "This device does not have a TouchID sensor.")
+        
+    }
+    
+    /**
+     This method will present an UIAlertViewController to inform the user that there was a problem with the TouchID sensor.
+     
+     - parameter error: the error message
+     
+     */
+    func showAlertViewAfterEvaluatingPolicyWithMessage( message:String ){
+        
+        showAlertWithTitle(title: "Error", message: message)
+        
+    }
+    
+    /**
+     This method presents an UIAlertViewController to the user.
+     
+     - parameter title:  The title for the UIAlertViewController.
+     - parameter message:The message for the UIAlertViewController.
+     
+     */
+    func showAlertWithTitle( title:String, message:String ) {
+        
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertVC.addAction(okAction)
+        
+        DispatchQueue.global(qos: .background).async {
+            
+            // Background Thread
+            
+            DispatchQueue.main.async {
+                // Run UI Updates
+                self.present(alertVC, animated: true, completion: {})
+            }
+        }
+        
+    }
+    
+    /**
+     This method will return an error message string for the provided error code.
+     The method check the error code against all cases described in the `LAError` enum.
+     If the error code can't be found, a default message is returned.
+     
+     - parameter errorCode: the error code
+     - returns: the error message
+     */
+    func errorMessageForLAErrorCode( errorCode:Int ) -> String{
+        
+        var message = ""
+        
+        switch errorCode {
+            
+        case LAError.appCancel.rawValue:
+            message = "Authentication was cancelled by application"
+            
+        case LAError.authenticationFailed.rawValue:
+            message = "The user failed to provide valid credentials"
+            
+        case LAError.invalidContext.rawValue:
+            message = "The context is invalid"
+            
+        case LAError.passcodeNotSet.rawValue:
+            message = "Passcode is not set on the device"
+            
+        case LAError.systemCancel.rawValue:
+            message = "Authentication was cancelled by the system"
+            
+        case LAError.touchIDLockout.rawValue:
+            message = "Too many failed attempts."
+            
+        case LAError.touchIDNotAvailable.rawValue:
+            message = "TouchID is not available on the device"
+            
+        case LAError.userCancel.rawValue:
+            message = "The user did cancel"
+            
+        case LAError.userFallback.rawValue:
+            message = "The user chose to use the fallback"
+            
+        default:
+            message = "Did not find error code on LAError object"
+            
+        }
+        
+        return message
+        
+    }
+    
+    /**
+     This method will push the authenticated view controller onto the UINavigationController stack
+     */
+
     
 }
