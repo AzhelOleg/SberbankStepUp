9905a1bcacde4ccf00d2bed3da56dbd5c172e347
AzhelOleg <azhelo@yandex.ru>
2019-11-16 12:37:40 +0300

    Add root

--- SberbankStepUp/SberbankStepUp/AppDelegate.swift
+++ SberbankStepUp/SberbankStepUp/AppDelegate.swift
@@ -12,12 +12,25 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
+        //
+		window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = RootViewController()
         window?.makeKeyAndVisible()
+		//
         return true
     }
-    
 }
+
+extension AppDelegate {
+    
+    public static var shared: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    public var rootViewController: RootViewController {
+        return window!.rootViewController as! RootViewController
+    }
+}
+
---
+++ SberbankStepUp/SberbankStepUp/RootViewController.swift
@@ -0,0 +1,86 @@
+//
+//  RootViewController.swift
+//  SberbankStepUp
+//
+//  Created by Oleg Azhel on 16.11.2019.
+//  Copyright © 2019 jorge. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    
+    private var current: UIViewController
+    
+    init() {
+        self.current = WelcomeViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+		//
+        addChild(current)
+        current.view.frame = view.bounds
+        view.addSubview(current.view)
+        current.didMove(toParent: self)
+    }
+    
+    public func showLoginScreen() {
+        let new = UINavigationController(rootViewController: LoginViewController())
+        
+        addChild(new)
+        new.view.frame = view.bounds
+        view.addSubview(new.view)
+        new.didMove(toParent: self)
+        //
+        current.willMove(toParent: nil)
+        current.view.removeFromSuperview()
+        current.removeFromParent()
+        //
+        current = new
+    }
+    
+    public func switchToMainScreen() {
+        animateFadeTransition(to: MyTabBarController())
+    }
+    
+    public func switchToLogout() {
+        let logoutScreen = UINavigationController(rootViewController: LoginViewController())
+        animateDismissTransition(to: logoutScreen)
+    }
+    
+
+    private func animateFadeTransition(to new: UIViewController, completion: (() -> Void)? = nil) {
+        current.willMove(toParent: nil)
+        addChild(new)
+        
+        transition(from: current, to: new, duration: 0.3, options: [.transitionCrossDissolve, .curveEaseOut], animations: {
+        }) { _ in
+            self.current.removeFromParent()
+            new.didMove(toParent: self)
+            self.current = new
+            completion?()
+        }
+    }
+    
+    private func animateDismissTransition(to new: UIViewController, completion: (() -> Void)? = nil) {
+//        _ = CGRect(x: -view.bounds.width, y: 0, width: view.bounds.width, height: view.bounds.height)
+        current.willMove(toParent: nil)
+        addChild(new)
+        transition(from: current, to: new, duration: 0.3, options: [], animations: {
+            new.view.frame = self.view.bounds
+        }) { _ in
+            self.current.removeFromParent()
+            new.didMove(toParent: self)
+            self.current = new
+            completion?()
+        }
+    }
+}
+
